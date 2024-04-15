bb2a04213b36ef2b8d07d0aebe1cea159c1240fc
mkajslaig <167091462+mkajslaig@users.noreply.github.com>
2024-04-15 19:13:32 +0700

    Create NGS.lua

---
+++ NGS.lua
@@ -0,0 +1,39 @@
+getgenv().Key = "ke722fae319d8c8ca8595862"
+getgenv().EquipMacroTroop = true
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1224650126518124585/1229181109087109180/Endlessfarm.json?ex=662ebf1d&is=661c4a1d&hm=e63eefd9e92603cdf14639631aca931bd8492cc7c84411d5977c11813debe25a&"
+getgenv().Config = {
+    ["AutoReplay"] = true,
+    ["AutoJoinMatch"] = false,
+    ["TPLobbyIfPlayer"] = true,
+    ["SelectMap"] = "Toilet City",
+    ["AutoJoinEndless"] = true,
+    ["AutoVoteDifficulty"] = "true",
+    ["SelectDifficulty"] = "Nightmare",
+    ["SelectMacro"] = "Endlessfarm",
+    ["AutoSkip"] = true,
+    ["DeleteMap"] = true,
+    ["PlayMacro"] = true,
+    ["AutoSave"] = true,
+    ["BoostFPS"] = true,
+    ["AutoClaimQuest"] = true,
+    ["AutoClaimEasterQuest"] = true,
+    ["AutoClaimEventPass"] = true,
+    ["AutoClaimVIP"] = false,
+    ["WhiteScreen"] = true,
+    ["DelayJoin"] = "45",
+    ["PlacingFailsafe"] = true,
+    ["ExchangeTier"] = "1",
+    ["AutoExchangeEgg"] = "true",
+    ["MailName"] = "",
+    ["RequiredGem"] = 1000,
+    ["AutoMail"] = false,
+    ["WH_MatchComplete"] = true,
+    ["AutoUseBoost"] = true,
+    ["UseBoosts"] = {
+        ["2xCoinsBoost"] = true,
+        ["2xEggsBoost"] = true,
+    },
+    ["UseAll"] = true,
+    ["WebhookURL"] = "https://discord.com/api/webhooks/1224737896163184661/5QXoPeEkTSf7iMOL0QZIXXtsTmrV-tXzLDh8a7jPgypM4l4fznYbDHR7hA-aZms6XWPe",
+}
+loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()
